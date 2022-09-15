f7cb22e17340c62bf13bed5197ffdeaf052619da
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-09-15 20:53:30 -0300

    feature: selectetapas created

---
+++ src/components/SelectEtapas/index.tsx
@@ -0,0 +1,32 @@
+import * as S from "./styles";
+import { cargos } from "../../mocks";
+
+interface SelectProps {
+  img?: string;
+  selected?: string;
+  setSalario?: any;
+  selectValue?: any;
+  setSelectValue?: any;
+}
+
+const Select = ({ img, selectValue, setSelectValue, }: SelectProps) => {
+  return (
+    <S.SelectOverlay>
+      <img src={img} alt="" />
+      <S.Select value={selectValue} onChange={e => setSelectValue(e.target.value)}>
+        <option selected disabled>
+          {selectValue}
+        </option>
+        {cargos.map((elem) => {
+          return (
+            <option key={elem.id} value={elem.name}>
+              {elem.name}
+            </option>
+          );
+        })}
+      </S.Select>
+    </S.SelectOverlay>
+  );
+};
+
+export default Select;
---
+++ src/components/SelectEtapas/styles.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const SelectOverlay = styled.div`
+  display: flex;
+  padding: 5px 10px;
+  background-color: #fff;
+  border: 1px #00000020 solid;
+  transition: 0.3s;
+  border-radius: 17px;
+  max-width: 400px;
+  :hover {
+    border: 1px solid #ea4f75;
+    box-shadow: 0px 0px 5px 1px #ea4f75;
+  }
+
+  img {
+    width: 30px;
+    background-color: #fff;
+  }
+`;
+
+export const Select = styled.select`
+  width: 900px;
+  height: 40px;
+  background-color: #fff;
+  border: none;
+  padding-left: 10px;
+  cursor: pointer;
+  font-size: 16px;
+
+  option {
+  padding: 10px;
+  color: black;
+
+}
+
+  :focus {
+    box-shadow: 0 0 0 0;
+    outline: 0;
+  }
+
+
+`;
